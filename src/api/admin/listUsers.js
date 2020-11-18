7155451a7fdbff9fd1e11db82b463c8b8fff0801
idbella <idbellasaid@gmail.com>
2020-11-18 01:33:47 +0100

    return phone number when fetching

--- src/api/admin/listUsers.js
+++ src/api/admin/listUsers.js
@@ -6,13 +6,13 @@
 /*   By: sid-bell <sid-bell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/08 15:24:25 by sid-bell          #+#    #+#             */
-/*   Updated: 2020/11/10 11:55:34 by sid-bell         ###   ########.fr       */
+/*   Updated: 2020/11/18 00:27:55 by sid-bell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 function listUsers(app, session, callback)
 {
-    const query = `select user.id, first_name,last_name, email, title, fk_role from user
+    const query = `select user.id, first_name,last_name, email, title, fk_role as role,phone from user
                     inner join role on role.id != 4 and user.fk_role=role.id`
     app.connection.query(query, callback);
 }
