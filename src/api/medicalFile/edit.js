4a441934b14914ce03f02506e6d5e4db72b373c0
idbella <idbellasaid@gmail.com>
2020-11-29 23:24:29 +0100

    return doctor id instead of user id when listing doctors

--- src/api/medicalFile/edit.js
+++ src/api/medicalFile/edit.js
@@ -6,7 +6,7 @@
 /*   By: sid-bell <sid-bell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 20:37:30 by sid-bell          #+#    #+#             */
-/*   Updated: 2020/11/11 10:14:26 by sid-bell         ###   ########.fr       */
+/*   Updated: 2020/11/29 23:23:28 by sid-bell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ function editMedicalFile(app, fileId, data, callback)
     const query = 'update medical_file set ? where id=?;'
 
     if (data.doctor)
-        newData.fk_doctor = data.fk_doctor 
+        newData.fk_doctor = data.doctor 
     if (data.insurance_type)
         newData.fk_insurance_type = data.insurance_type
     app.connection.query(query, [newData, fileId], callback)
--- src/api/user/listDoctors.js
+++ src/api/user/listDoctors.js
@@ -6,12 +6,12 @@
 /*   By: sid-bell <sid-bell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/22 09:05:00 by sid-bell          #+#    #+#             */
-/*   Updated: 2020/11/22 09:35:39 by sid-bell         ###   ########.fr       */
+/*   Updated: 2020/11/29 22:42:13 by sid-bell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 module.exports = (app, callback) => {
 
-    const query = 'select user.id, first_name, last_name, email, creation_date, phone, speciality from user inner join doctor on doctor.fk_user = user.id'
+    const query = 'select doctor.id, first_name, last_name, email, creation_date, phone, speciality from user inner join doctor on doctor.fk_user = user.id'
     app.connection.query(query, callback)
 }
