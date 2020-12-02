e09d18ba8ea1815be5c8e9e498f0fee8b5b7b6e1
said id-bella ali <idbellasaid@gmail.com>
2020-12-02 15:31:55 +0100

    Allow all origins

--- src/initServer.js
+++ src/initServer.js
@@ -23,7 +23,15 @@ const jwt           = require('jsonwebtoken')
 const connection    = mysql.createPool(process.env.CLEARDB_DATABASE_URL);
 
 const app = express();
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "OPTIONS, POST, GET, PUT, DELETE");
+  if ('OPTIONS' == req.method) {
+      return res.sendStatus(200);
+  } else {
+      next();
+  }
+});
 app.use(cors({
     origin: function(origin, callback){
       return callback(null, true);
