bb04b331aa49a199210372948e4ec317e8119b30
idbella <idbellasaid@gmail.com>
2020-12-05 09:51:20 +0100

    add permissions field to newly added notes

--- src/api/notes/addNote.js
+++ src/api/notes/addNote.js
@@ -6,7 +6,7 @@
 /*   By: sid-bell <sid-bell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/12 14:58:40 by sid-bell          #+#    #+#             */
-/*   Updated: 2020/11/12 15:16:57 by sid-bell         ###   ########.fr       */
+/*   Updated: 2020/12/04 12:20:38 by sid-bell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ module.exports = (app, userId, fileId, data, callback) => {
     const newData = {
             notes           :data.notes,
             fk_medical_file :fileId,
-            fk_user         :userId
+            fk_user         :userId,
+            permissions     :data.permissions
         }
+    console.log(newData)
     app.connection.query(query, newData, callback)
 }
