39ff659671f2b1e1f917a83020b1165152b7f4b9
idbella <idbellasaid@gmail.com>
2020-12-05 09:50:36 +0100

    change users permission to support bitwise operator check

--- src/api/const/roles.js
+++ src/api/const/roles.js
@@ -6,14 +6,14 @@
 /*   By: sid-bell <sid-bell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 19:46:55 by sid-bell          #+#    #+#             */
-/*   Updated: 2020/11/10 22:55:43 by sid-bell         ###   ########.fr       */
+/*   Updated: 2020/12/04 10:53:49 by sid-bell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 module.exports =    {
-						admin		:{id:1,permission:16384},
-						doctor		:{id:2,permission:1792},
-						nurse		:{id:3,permission:48},
+						admin		:{id:1,permission:1},
+						doctor		:{id:2,permission:2},
+						nurse		:{id:3,permission:4},
 						patient		:{id:4,permission:8},
-						receptionist:{id:5,permission:0}
+						receptionist:{id:5,permission:16}
 					}
