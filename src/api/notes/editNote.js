82415ae813bd09fb70cd2a5098267e15628740ff
idbella <idbellasaid@gmail.com>
2020-12-05 09:53:23 +0100

    when fetching notes the api now return only the note that are accessible to the current logged in user

--- src/api/notes/editNote.js
+++ src/api/notes/editNote.js
@@ -6,12 +6,12 @@
 /*   By: sid-bell <sid-bell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/12 15:15:33 by sid-bell          #+#    #+#             */
-/*   Updated: 2020/11/12 15:23:52 by sid-bell         ###   ########.fr       */
+/*   Updated: 2020/12/04 12:04:03 by sid-bell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 module.exports = (app, userId, noteId, data, callback) => {
 
-    const query = 'update note set `notes`=? where note.id=? and fk_user=?;'
-    app.connection.query(query, [data.notes, noteId, userId], callback)
+    const query = 'update note set `notes`=?,permissions=? where note.id=? and fk_user=?;'
+    app.connection.query(query, [data.notes, data.permissions, noteId, userId], callback)
 }
--- src/api/notes/getNotes.js
+++ src/api/notes/getNotes.js
@@ -6,11 +6,18 @@
 /*   By: sid-bell <sid-bell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/13 10:02:36 by sid-bell          #+#    #+#             */
-/*   Updated: 2020/11/30 14:41:19 by sid-bell         ###   ########.fr       */
+/*   Updated: 2020/12/04 12:47:18 by sid-bell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-module.exports = (app, fileId, callback) => {
-    const query = 'select user.title, user.fk_role, first_name,last_name, note.id,note.creation_date as date,notes,fk_user as userId from note inner join user on fk_medical_File = ? and user.id = note.fk_user;'
-    app.connection.query(query, fileId, callback)
+module.exports = (app, user, fileId, callback) => {
+
+    const role = user.role
+    const id = user.userId
+
+    const query = 'select user.id as userId, user.title, user.fk_role,\
+                first_name,last_name, note.id,note.creation_date as date,notes,\
+                fk_user as userId, permissions from note inner join user on fk_medical_File = ? and user.id = note.fk_user\
+                and (note.fk_user = ? or note.permissions & ?);'
+    app.connection.query(query, [fileId, id, role], callback)
 }
