a4be7b90e22bcb34a9c647065949b0c30cb4390c
idbella <idbellasaid@gmail.com>
2020-12-23 17:51:58 +0100

    fix patients not listed for nurses when selecting patients + fix duplicate patients selection

--- src/api/patient/list.js
+++ src/api/patient/list.js
@@ -6,7 +6,7 @@
 /*   By: sid-bell <sid-bell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/12 10:55:02 by sid-bell          #+#    #+#             */
-/*   Updated: 2020/12/19 18:46:53 by sid-bell         ###   ########.fr       */
+/*   Updated: 2020/12/23 17:49:58 by sid-bell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,36 +15,18 @@ const roles = require("../const/roles")
 
 function listPatients(app, user, callback){
 
-	var query = 'SELECT nurses, user.id as userid,user.email, patient.address, user.phone, user.first_name, user.last_name, patient.cin, patient.id as patientid,\
-					country, postalcode, city,birthday,sexe from patient inner join user on user.id = patient.fk_user INNER JOIN medical_file on medical_file.fk_patient=patient.id'
+	var query = 'SELECT user.id as userid,user.email, patient.address, user.phone, user.first_name, user.last_name, patient.cin, patient.id as patientid,\
+					country, postalcode, city,birthday,sexe from patient inner join user on user.id = patient.fk_user\
+					WHERE EXISTS (select 1 from medical_file where medical_file.fk_patient=patient.id)'
 	if (user.role == roles.doctor.id)
-		query += '  and medical_file.fk_doctor= ' + user.userId
+		query += '  and EXISTS (select medical_file.id from medical_file where medical_file.fk_doctor= ' + user.userId + ')'
 	if (user.role == roles.nurse.id)
-		query += ' INNER join nurses on nurses.fk_medical_file = medical_file.id and nurses.fk_user = ' + user.userId
+		query += ' and EXISTS (select 1 from medical_file inner join nurses on nurses.fk_medical_file = medical_file.id and nurses.fk_user = ' + user.userId + ')'
 	query += ';'
-
+	console.log(query)
 	app.connection.query(query, (err,result)=>{
 		if (err)
 			return (callback(err))
-		if (user.role == roles.nurse.id)
-		{
-			var patients = [];
-			if (result.length > 0)
-			{
-				result.forEach(patient => {
-					if (patient.nurses)
-					{
-						nurses = patient.nurses.split(',')
-						nurses.forEach(nurseId=>{
-							console.log(nurseId + ' vs ' + user.userId)
-							if (nurseId == user.userId)
-								patients.push(patient)
-						})
-					}
-				});
-			}
-			return callback(err, patients)
-		}
 		callback(err, result)
 	})
 }
